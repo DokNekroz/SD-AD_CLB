d38747ec0e15804483201883d5e017ce1d43dc4b
DokNekroz <revinski.d.o@gmail.com>
2025-07-27 21:43:06 +0000

    0.04

--- setup_sd_animatediff.sh
+++ setup_sd_animatediff.sh
@@ -7,6 +7,11 @@ set -e
 export COMMANDLINE_ARGS="--skip-torch-cuda-test --no-half --precision full --use-cpu all --upcast-sampling"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 
+#MEGA install 
+wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb
+apt install ./megacmd-xUbuntu_20.04_amd64.deb -y
+mega-login "{mega_email}" "{mega_password}"
+
 # 2. Клонирование stable-diffusion-webui
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 cd stable-diffusion-webui
@@ -14,6 +19,7 @@ cd stable-diffusion-webui
 # 3. Установка AnimateDiff
 mkdir -p extensions
 git clone https://github.com/continue-revolution/sd-webui-animatediff.git extensions/sd-webui-animatediff
+
 
 # 4. Установка motion-модуля
 mkdir -p models/MotionModules
