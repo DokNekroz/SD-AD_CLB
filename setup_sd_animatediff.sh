11205bd5046b8754a162427efd6fe354fe7c979c
DokNekroz <revinski.d.o@gmail.com>
2025-07-28 12:44:59 +0000

    0.047

--- setup_sd_animatediff.sh
+++ setup_sd_animatediff.sh
@@ -21,21 +21,24 @@ apt update && apt install -y megatools
 # 1. Models
 mkdir -p /content/temp_models && cd /content/temp_models
 megadl 'https://mega.nz/folder/Zm4gzIrD#UcuYvAvSxBDAJKbQAER0sg'
-rm -rf /content/SD-AD_CLB/stable-diffusion-webui/models
-mkdir -p /content/SD-AD_CLB/stable-diffusion-webui/models
+rm -rf /content/SD-AD_CLB/stable-diffusion-webui/models/
+mkdir -p /content/SD-AD_CLB/stable-diffusion-webui/models/
 cd /content/temp_models
 mv -f * /content/SD-AD_CLB/stable-diffusion-webui/models/
 cd /content && rm -rf /content/temp_models
 
 
 # 3. Embeddings
-#mkdir -p /content/temp_embeddings && cd /content/temp_embeddings
-#megadl 'https://mega.nz/folder/MjBDGQ6A#2T485PZ8aYWpILuLWUKo4A'
-#mv -f * /content/SD-AD_CLB/stable-diffusion-webui/embeddings/
-#cd /content && rm -rf /content/temp_embeddings
+mkdir -p /content/temp_embeddings && cd /content/temp_embeddings
+megadl 'https://mega.nz/folder/MjBDGQ6A#2T485PZ8aYWpILuLWUKo4A'
+rm -rf /content/SD-AD_CLB/stable-diffusion-webui/embeddings/
+mkdir -p /content/SD-AD_CLB/stable-diffusion-webui/embeddings/
+cd /content/temp_embeddings
+mv -f * /content/SD-AD_CLB/stable-diffusion-webui/embeddings/
+cd /content && rm -rf /content/temp_embeddings
 
 # 5. Установка зависимостей — лучше в Colab запускать из Jupyter, не здесь
-# cd /content/SD-AD_CLB/stable-diffusion-webui
+cd /content/SD-AD_CLB/stable-diffusion-webui
 pip install -r requirements.txt
 # 6. Запуск интерфейса
 COMMANDLINE_ARGS="--xformers --no-half --disable-safe-unpickle --enable-insecure-extension-access --allow-code --medvram --opt-sdp-attention --precision full --upcast-sampling --skip-torch-cuda-test --listen --share" \
