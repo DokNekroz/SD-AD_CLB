7b85025351389637dea4b5472189e6e6c96d318f
DokNekroz <revinski.d.o@gmail.com>
2025-07-28 11:42:34 +0000

    0.06

--- setup_sd_animatediff.sh
+++ setup_sd_animatediff.sh
@@ -29,14 +29,14 @@ apt update && apt install -y megatools
 # 1. Models
 mkdir -p /content/temp_models && cd /content/temp_models
 megadl 'https://mega.nz/folder/Zm4gzIrD#UcuYvAvSxBDAJKbQAER0sg'
-mv -f * /content/SD-AD_CLB/stable-diffusion-webui
+mv -f * /content/SD-AD_CLB/stable-diffusion-webui/
 cd /content && rm -rf /content/temp_models
 
 
 # 3. Embeddings
 mkdir -p /content/temp_embeddings && cd /content/temp_embeddings
 megadl 'https://mega.nz/folder/MjBDGQ6A#2T485PZ8aYWpILuLWUKo4A'
-mv -f * /content/stable-diffusion-webui/embeddings/
+mv -f * /content/SD-AD_CLB/stable-diffusion-webui/embeddings/
 cd /content && rm -rf /content/temp_embeddings
 
 # 5. Установка зависимостей — лучше в Colab запускать из Jupyter, не здесь
